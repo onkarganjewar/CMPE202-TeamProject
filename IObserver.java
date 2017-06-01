6038eaca2960d23ae1df0fcf1fd6140699c4f949
Onkar Ganjewar <ganjewaronkar@gmail.com>
2017-05-31 18:45:21 -0700

    Update .gitignore

--- IObserver.java
+++
@@ -1,8 +0,0 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
-public interface IObserver {
-     
-    //method to update the observer, used by subject
-    public void update(Board world);
-     
-}
